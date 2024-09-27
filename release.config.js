b62ddd83401cc0fc26e61ec9fa317a68d5783e42
William Killerud <william.killerud@schibsted.com>
2024-09-27 15:06:49 +0200

    chore: use shared semrel config

--- release.config.js
+++ release.config.js
@@ -1,22 +1,3 @@
 export default {
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        [
-            '@semantic-release/npm',
-            {
-                tarballDir: 'release',
-            },
-        ],
-        [
-            '@semantic-release/github',
-            {
-                assets: 'release/*.tgz',
-            },
-        ],
-        '@semantic-release/git',
-    ],
-    preset: 'angular',
-    branches: [{ name: 'main' }, { name: 'next', prerelease: true }],
+    extends: '@podium/semantic-release-config',
 };
